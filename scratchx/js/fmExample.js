f6593e0af93638f3ce85b7713592c3d8e31d710d
fongshek <info.fongming@gmail.com>
2015-08-10 17:16:40 +0800

    add power , report

--- scratchx/js/fmExample.js
+++ scratchx/js/fmExample.js
@@ -35,11 +35,30 @@
 		});
 	};	
 
+    // Functions for block with type 'w' will get a callback function as the 
+    // final argument. This should be called to indicate that the block can
+    // stop waiting.
+    ext.wait_random = function(callback) {
+        wait = Math.random();
+        console.log('Waiting for ' + wait + ' seconds');
+        window.setTimeout(function() {
+            callback();
+        }, wait*1000);
+    };
+
+
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['R', '?%s from fm ID %n', 'grab'],
             ['R', 'temperature in %s', 'get_temp', 'Hong Kong'],
+            ['w', 'wait for random time', 'wait_random'],
+            // Block type, block name, function name, param1 default value, param2 default value
+            ['r', '%n ^ %n', 'power', 2, 3],			
         ]
     };
 
